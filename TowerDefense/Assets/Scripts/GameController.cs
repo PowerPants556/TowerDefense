1a117372380eee84128e4985d4a72f093b5ca4cc
PowerPants556 <146521497+PowerPants556@users.noreply.github.com>
2024-09-14 12:29:12 +0300

    TowerDefence

--- TowerDefense/Assets/Scripts/GameController.cs
+++ TowerDefense/Assets/Scripts/GameController.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    private const int NODE_GRID_ROW_COUNT = 4;
+    [Header("Settings")]
+    [SerializeField] private int NODE_GRID_ROW_COUNT = 4;
+    [SerializeField] private int NODE_GRID_COLLUM_COUNT = 4;
+    [Tooltip("Ofset between nodes")]
+    [SerializeField] private float offset = 1.5f;
+
+    [Space(15)]
+    [Header("Prefabs: ")]
     [SerializeField] private GameObject nodePrefab;
 
     private void Start()
     {
-        CreateNodes();
-    }
 
+    }
+    
+    [ContextMenu("CreateNodes")]
     private void CreateNodes()
     {
+        if (transform.childCount > 0)
+        {
+            /*foreach(Transform child in transform.GetComponentsInChildren<Transform>())
+            {
+                DestroyImmediate(child);
+            }*/
+        }
         for (int x = 0; x < NODE_GRID_ROW_COUNT; x++)
         {
-            for(int z = 0; z <NODE_GRID_ROW_COUNT; z++)
+            for(int z = 0; z <NODE_GRID_COLLUM_COUNT; z++)
             {
-                Instantiate(nodePrefab, new Vector3(x, 0, z), Quaternion.identity);
+                GameObject obj = Instantiate(nodePrefab, new Vector3(x * offset, 0, z * offset), Quaternion.identity, transform);
+                obj.name = "Name: " + x + " " + z;
             }
         }
     }
